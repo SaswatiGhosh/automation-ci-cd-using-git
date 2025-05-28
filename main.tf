1fe41d2163a7c0a616cd2e99a74306fe2dd7887f
SaswatiGhosh <saswatititli99@gmail.com>
2025-05-28 18:41:02 +0530

    made small changes in main.tf

--- main.tf
+++ main.tf
@@ -52,13 +52,13 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = data.archive_file.output_path
+  filename      = data.archive_file.lambda.output_path
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_handler"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "python13.3"
+  runtime = "python3.13"
 
 }

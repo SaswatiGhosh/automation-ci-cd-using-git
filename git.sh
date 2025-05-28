5b79d3912c68caa8286071d88152a65ddd77d97c
SaswatiGhosh <saswatititli99@gmail.com>
2025-05-28 19:40:29 +0530

    Automated commit from CI/CD pipeline

--- git.sh
+++ git.sh
@@ -9,9 +9,9 @@ current_status=$(git status 2>&1)
 while true
 do
     if [ "$current_status" == "nothing to commit, working tree clean" ]; then
-    echo "The repository is clean."
     
-    else 
+    elseecho "The repository is clean."
+     
     echo "Git status is not as expected.Change is there."
     git add .
     git commit -m "Automated commit from CI/CD pipeline"
